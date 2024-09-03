90c29978d3529ca1fc71887d5180e286784c8627
AndjDavis <andi.idavis@gmail.com>
2024-09-03 11:55:47 -0600

    basic styles for card list applied

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import TaskList from "@/app/ui/tasks/task-list";
 export default function Home() {
   return (
     <main className="min-h-screen relative">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg w-1/4 text-center">
-        <div className="flex flex-col items-center justify-between">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-2xl bg-gray-50 rounded-lg w-1/4 h-2/4 text-center">
           <TaskList />
-        </div>
       </div>
     </main>
   );
--- app/ui/tasks/task-list.tsx
+++ app/ui/tasks/task-list.tsx
@@ -1,8 +1,18 @@
+import { fetchAllTasks } from "@/app/lib/data";
+import { ITask } from "@/app/lib/definitions";
+import { Card } from "@/app/ui/card";
 
 export default async function TaskList() {
+  const tasks: ITask[] = await fetchAllTasks();
+
   return (
-    <>
-      <h2>Task List</h2>
-    </>
+    <div className="flex flex-col justify-start w-full h-full">
+      <h2 className="flex flex-col justify-center h-1/6 font-bold text-2xl md:text-2xl">
+        Task List
+      </h2>
+      <div className="flex flex-col h-5/6 overflow-x-auto">
+        {tasks.map((task) => <Card key={task.id} task={task} />)}
+      </div>
+    </div>
   );
 };
